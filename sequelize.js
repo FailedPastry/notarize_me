5477dc12e60bff9f01bb17f3d68f3b50e9a141f8
Cedric Simpkins <126218396+github-cedsim@users.noreply.github.com>
2024-04-10 20:21:42 -0500

    Delete sequelize.js

--- sequelize.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Replace 'database', 'username', and 'password' with your MySQL details
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
